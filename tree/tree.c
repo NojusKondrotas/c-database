9dc39b147d3781effbb15960849984413ff759dd
NojusKondrotas <nojus.kondrotas23@gmail.com>
2025-03-12 19:49:07 +0200

    add structure

---
+++ tree/tree.c
@@ -0,0 +1,8 @@
+//programming implementation
+
+#include "tree.h"
+
+int main(){
+    printf("Hello World!\n");
+    return 0;
+}
---
+++ tree/tree.h
@@ -0,0 +1,11 @@
+//definitions
+
+#define GNU_SOURCE
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <assert.h>
+#include <errno.h>
