8f7c4a9076ae71df1a4c4f96bd66b65edfacea6b
NojusKondrotas <nojus.kondrotas23@gmail.com>
2025-03-12 20:01:55 +0200

    add variable typedefs

--- tree/tree.h
+++ tree/tree.h
@@ -15,8 +15,13 @@ Tree
     Node, Leaf
 */
 
+typedef unsigned int int32;
+typedef unsigned short int int16;
+typedef unsigned char int8;
+
 struct s_node{
     struct s_node *north;
     struct s_node *west;
     struct s_leaf *east;
+    int8 path[256];
 };
